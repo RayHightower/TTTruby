49a3fbf633a156bbbd26b24281b19330c7fd391f
Raymond T. Hightower <rth@wisdomgroup.com>
2016-11-29 13:54:25 -0600

    Install bundle and guard to get this red/green/refactor rhythm going.

---
+++ spec/game_spec.rb
@@ -0,0 +1,18 @@
+require 'game'
+
+module TTTruby
+
+  describe Game do
+    context "orginary game of Tic Tac Toe" do
+
+      before do
+        @game = Game.new
+        @cells = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+      end
+
+      it "creates a grid with the right number of cells" do
+        @game.cells
+      end
+    end
+  end
+end
