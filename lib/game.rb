1cf0ab7b0377d5ff8eadbec0e84b26daabf47b15
Raymond T. Hightower <rth@wisdomgroup.com>
2016-11-29 14:28:52 -0600

    Create one game with one grid and two players.

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,12 @@
-require 'grid'
+require_relative './grid'
+require_relative './player'
 
 class Game
 
   def initialize
     @grid = Grid.new
-
-
+    @playerX = Player.new
+    @player0 = Player.new
   end
 
   def print_status
---
+++ lib/grid.rb
@@ -0,0 +1,8 @@
+class Grid
+
+  def initialize
+    @cells = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+  end
+
+end
+
