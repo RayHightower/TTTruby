c39ca97785c5fd79b8aa0561bb47ac79d22a24db
Raymond T. Hightower <rth@wisdomgroup.com>
2016-12-01 10:21:43 -0600

    Initialize and print the grid successfully.

--- lib/grid.rb
+++ lib/grid.rb
@@ -4,12 +4,22 @@ class Grid
     @cell = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
   end
 
+  def contents
+    @cell
+  end
+
   def print_grid
-    print "#{@cell[1]} | #{@cell[2]} | #{@cell[3]}"
-    print "------------"
+    print " #{@cell[1]} | #{@cell[2]} | #{@cell[3]}"
+    print "\n-----------\n"
+    print " #{@cell[4]} | #{@cell[5]} | #{@cell[6]}"
+    print "\n-----------\n"
+    print " #{@cell[7]} | #{@cell[8]} | #{@cell[9]}"
+    print "\n"
   end
 end
 
-print_grid
+sample_grid = Grid.new
+sample_grid.print_grid
 
+sample_grid.contents
 
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -3,10 +3,15 @@ require 'grid'
 describe Grid do
   context "what the grid should look like" do
 
-    it "prints a horizontal line for the grid"
+    it "creates the initial TTT grid" do
+      sample_grid = Grid.new
+      expect(sample_grid.contents).to eq [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+    end
 
-    it "displays the contents of the tic-tac-toe grid"
+    it "prints a horizontal line for the grid" do
+    end
 
-
+    it "displays the contents of the tic-tac-toe grid" do
+    end
   end
 end
