83b8882702093856d00377196ce0f3f7d367199a
Raymond T. Hightower <rth@wisdomgroup.com>
2016-12-01 10:02:57 -0600

    Start printing the grid.

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,8 +1,15 @@
 class Grid
 
   def initialize
-    @cells = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+    @cell = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
   end
 
+  def print_grid
+    print "#{@cell[1]} | #{@cell[2]} | #{@cell[3]}"
+    print "------------"
+  end
 end
+
+print_grid
+
 
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -3,6 +3,8 @@ require 'grid'
 describe Grid do
   context "what the grid should look like" do
 
+    it "prints a horizontal line for the grid"
+
     it "displays the contents of the tic-tac-toe grid"
 
 
