db1d21e237dd96be42234981ac7cb5fdb73e1fec
Raymond T. Hightower <rth@wisdomgroup.com>
2016-12-13 14:16:14 -0600

    Maybe we nailed the off-by-one?

--- lib/main.rb
+++ lib/main.rb
@@ -12,7 +12,7 @@ player[1] = Player.new("O")
 puts "\nWelcome to Tic Tac Toe!\nYou know the rules. Here's the grid.\n"
 thisgame.print_color_grid
 
-turn = 0
+turn = 1
 game_is_active = true
 
 # Main event loop.
