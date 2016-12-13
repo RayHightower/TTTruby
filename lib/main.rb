2abc9f1159eab38b693948820678985eb5d753f8
Raymond T. Hightower <rth@wisdomgroup.com>
2016-12-13 11:04:05 -0600

    Use instance variables only where needed.

--- lib/main.rb
+++ lib/main.rb
@@ -4,27 +4,28 @@
 require_relative './game'
 
 # Setup for main event loop.
-@thisgame = Game.new
-@player = []
-@player[0] = Player.new("X")
-@player[1] = Player.new("O")
+thisgame = Game.new
+player = []
+player[0] = Player.new("X")
+player[1] = Player.new("O")
 
 puts "\nWelcome to Tic Tac Toe\nYou know the rules. Here's the grid.\n"
-@thisgame.print_grid
+thisgame.print_grid
 
 turn = 0
 game_is_active = :true
 
 # Main event loop.
 while game_is_active
-  turn = turn + 1
-  mover = @player[turn % 2]
+  mover = player[turn % 2]
   move = gets.chomp
-  puts "Okay #{mover.designation}, your move is #{move}."
+  puts "You placed your #{mover.designation} at position #{move}."
+  # thisgame.add_move(move)
   # @thisgame.get_move(mover)
 
   # if @thisgame.check_winner
   #   puts "*** #{mover} wins! ***"
   #   break
   # end
+  turn = turn + 1
 end
