ef14285177bdbecdb3fac680da6b0b12b7abb458
Raymond T. Hightower <rth@wisdomgroup.com>
2016-12-13 10:57:02 -0600

    Get the main event loop to function.

--- lib/main.rb
+++ lib/main.rb
@@ -6,13 +6,14 @@ require_relative './game'
 # Setup for main event loop.
 @thisgame = Game.new
 @player = []
-@player[0] = Player.new
-@player[1] = Player.new
+@player[0] = Player.new("X")
+@player[1] = Player.new("O")
 
 puts "\nWelcome to Tic Tac Toe\nYou know the rules. Here's the grid.\n"
 @thisgame.print_grid
 
 turn = 0
+game_is_active = :true
 
 # Main event loop.
 while game_is_active
@@ -21,8 +22,8 @@ while game_is_active
   move = gets.chomp
   puts "Okay #{mover.designation}, your move is #{move}."
   # @thisgame.get_move(mover)
-  
-  # if @thisgame.check_winner 
+
+  # if @thisgame.check_winner
   #   puts "*** #{mover} wins! ***"
   #   break
   # end
