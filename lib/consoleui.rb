3a9b5638c266f97d24b48d451420fdbd6061b815
Raymond T. Hightower <rth@wisdomgroup.com>
2016-12-22 12:56:48 -0600

    Break out the console UI to make it easier to test.

---
+++ lib/consoleui.rb
@@ -0,0 +1,3 @@
+class ConsoleUI
+
+end
---
+++ spec/consoleui_spec.rb
@@ -0,0 +1,19 @@
+require 'consoleui'
+
+describe ConsoleUI do
+  context "this is how the players interact with the game" do
+
+    before do
+
+    end
+
+    it "accepts a move from a player" do
+
+    end
+
+    it "prints the grid on the standard output for the UI" do
+
+    end
+
+  end
+end
