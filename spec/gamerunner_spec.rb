e0bb01b2c532ace26d00411246ed8a6123ccc22e
Raymond T. Hightower <rth@wisdomgroup.com>
2016-12-28 09:40:59 -0600

    Where does the StringIO class fit? How can it help us to write tests involving characters read from the command line?

---
+++ spec/gamerunner_spec.rb
@@ -0,0 +1,21 @@
+require 'gamerunner'
+
+describe Gamerunner do
+  context "entry point for the TTT game" do
+    before do
+      @game = Game.new
+      @player = [Player.new("X"), Player.new("O")]
+    end
+
+    it "creates a game with two players" do
+      thisgame = Game.new
+      player_bench = [Player.new("X"), Player.new("O")]
+      expect(player_bench[0].designation).to eq "X"
+    end
+
+    it "receives a move and stores it" do
+
+    end
+
+  end
+end
