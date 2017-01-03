e00e872df2ef8d52e32008f4e23b56311bcb0e3a
Raymond T. Hightower <rth@wisdomgroup.com>
2017-01-02 23:39:52 -0600

    Tests are passing without us adding to Gamerunner class. Only adding to Game and Player classes so far. Is this a smell?

--- spec/gamerunner_spec.rb
+++ spec/gamerunner_spec.rb
@@ -4,13 +4,19 @@ describe Gamerunner do
   context "entry point for the TTT game" do
     before do
       @game = Game.new
-      @player = [Player.new("X"), Player.new("O")]
+      @game.player = [Player.new("X"), Player.new("O")]
     end
 
-    it "creates a game with two players" do
+    it "creates a game with player named 'X'" do
       thisgame = Game.new
-      player_bench = [Player.new("X"), Player.new("O")]
-      expect(player_bench[0].designation).to eq "X"
+      thisgame.player = [Player.new("X"), Player.new("O")]
+      expect(thisgame.player[0].designation).to eq "X"
+    end
+
+    it "creates a game with player named 'O'" do
+      thisgame = Game.new
+      thisgame.player = [Player.new("X"), Player.new("O")]
+      expect(thisgame.player[1].designation).to eq "O"
     end
 
     it "receives a move and stores it" do
