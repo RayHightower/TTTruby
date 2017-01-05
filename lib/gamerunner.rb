9f707937b2906fc8f793b23087a4f455ccc27292
Raymond T. Hightower <rth@wisdomgroup.com>
2017-01-04 18:34:55 -0600

    Typo correction.

--- lib/gamerunner.rb
+++ lib/gamerunner.rb
@@ -1,5 +1,13 @@
 require_relative './game'
 
 class Gamerunner
+  attr_accessor :game
+
+  def initialize
+    @game = Game.new
+    @game.player = [Player.new("X"), Player.new("O")]
+  end
+
+
 
 end
--- spec/gamerunner_spec.rb
+++ spec/gamerunner_spec.rb
@@ -3,25 +3,22 @@ require 'gamerunner'
 describe Gamerunner do
   context "entry point for the TTT game" do
     before do
-      @game = Game.new
-      @game.player = [Player.new("X"), Player.new("O")]
+
     end
 
     it "creates a game with player named 'X'" do
-      thisgame = Game.new
-      thisgame.player = [Player.new("X"), Player.new("O")]
-      expect(thisgame.player[0].designation).to eq "X"
+      thisgamerunner = Gamerunner.new
+      expect(thisgamerunner.game.player[0].designation).to eq "X"
     end
 
     it "creates a game with player named 'O'" do
-      thisgame = Game.new
-      thisgame.player = [Player.new("X"), Player.new("O")]
-      expect(thisgame.player[1].designation).to eq "O"
+      thisgamerunner = Gamerunner.new
+      expect(thisgamerunner.game.player[1].designation).to eq "O"
     end
 
     it "receives a move and stores it" do
       moves = StringIO.new('123456789')
-      expect(true).to eq true
+      expect(true).to eq false
     end
 
   end
