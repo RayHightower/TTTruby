aaeaa8b551117d741498225dbbb4406ecaccf26a
Raymond T. Hightower <rth@wisdomgroup.com>
2017-01-04 19:39:12 -0600

    Add GameRunner test to determine whether X has won the game.

--- spec/gamerunner_spec.rb
+++ spec/gamerunner_spec.rb
@@ -4,31 +4,35 @@ describe GameRunner do
   context "entry point for the TTT game" do
     before do
       # Do we really need to set anything up in this case?
+      @thisgamerunner = GameRunner.new
+      @players = @thisgamerunner.game.player
+      @bunch_of_moves = StringIO.new('123456789')
     end
 
     it "creates a game with player named 'X'" do
-      thisgamerunner = GameRunner.new
-      expect(thisgamerunner.game.player[0].designation).to eq "X"
+      expect(@thisgamerunner.game.player[0].designation).to eq "X"
     end
 
     it "creates a game with player named 'O'" do
-      thisgamerunner = GameRunner.new
-      expect(thisgamerunner.game.player[1].designation).to eq "O"
+      expect(@thisgamerunner.game.player[1].designation).to eq "O"
     end
 
     it "stores the state of the game" do
-      thisgamerunner = GameRunner.new
-      currentgrid = thisgamerunner.get_status
+      currentgrid = @thisgamerunner.get_status
       expect(currentgrid).to eq [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
     end
 
-    it "can tell if X has won the game" do
-
+    it "receives a move and stores it" do
+      player = @players[0]
+      move = @bunch_of_moves.getc.to_i
+      @thisgamerunner.game.add_move(player, move)
+      currentgrid = @thisgamerunner.get_status
+      expect(currentgrid).to eq [ 0, "X", 2, 3, 4, 5, 6, 7, 8, 9 ]
     end
 
-    it "receives a move and stores it" do
-      moves = StringIO.new('123456789')
-      expect(true).to eq false
+    it "can tell if X has won the game" do
+      winning_moves_for_x = StringIO.new('15293')
+      expect(@thisgamerunner.game.check_winner).to eq true
     end
 
   end
