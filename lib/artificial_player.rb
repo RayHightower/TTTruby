78a9dceb1e0c580cbb957698ba9147275b4a86e4
Raymond T. Hightower <rth@wisdomgroup.com>
2017-02-19 15:38:30 -0600

    Focus on the minimax algorithim as a method employed by the Artificial_Player object.

--- lib/artificial_player.rb
+++ lib/artificial_player.rb
@@ -1,7 +1,13 @@
+require_relative './grid'
+
 class ArtificialPlayer
 
   def get_move
     1
+  end
+
+  def minimax(current_grid, current_player)
+
   end
 
 end
--- spec/artificial_player_spec.rb
+++ spec/artificial_player_spec.rb
@@ -1,4 +1,5 @@
 require 'artificial_player'
+# Artificial Player will make the best available move based on the minimax algorithm.
 
 describe ArtificialPlayer do
   context "provide the best move from available choices" do
@@ -7,13 +8,17 @@ describe ArtificialPlayer do
       @allowed_moves = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
     end
 
-    it "returns an integer when given a board & current player" do
-
-    end
-
     it "gives a move when we ask for a move" do
+      # This is a throwaway test. 
       move = @art.get_move
       expect(@allowed_moves.include? move).to eq true
     end
+
+    it "returns an integer when given a board & current player" do
+
+      move = minimax(@current_grid, @current_player)
+
+    end
+
   end
 end
