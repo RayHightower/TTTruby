df499b4c19bd6d4598fa8960d091d271dbc4a4dc
Raymond T. Hightower <rth@wisdomgroup.com>
2017-02-20 16:48:09 -0600

    Quit string works. Now ensuring that the state changes with each turn. I think that the problem is in the test, not the production code.

--- spec/gamerunner_spec.rb
+++ spec/gamerunner_spec.rb
@@ -68,7 +68,7 @@ describe GameRunner do
       @thisgamerunner.next_turn
       grid_state_2 = @thisgamerunner.get_status
       # puts "\ngrid_state_1 = #{grid_state_1}"
-      expect(grid_state_1).not_to eq grid_state_2
+      expect(grid_state_1 == grid_state_2).to eq false
     end
   end
 end
