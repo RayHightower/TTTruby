5e3ec80c35cc3c4139166a7c49c0ef45e9e92096
Raymond T. Hightower <rth@wisdomgroup.com>
2017-03-21 23:07:26 -0500

    Leave some failing tests so we know where to start next time.

--- lib/player.rb
+++ lib/player.rb
@@ -12,20 +12,23 @@ class Player
   end
 
   def get_move(current_grid)
-    best_score = 0
-    best_move = 0
-    score_array = current_grid.empty_cell_list
+    best_move = 0    # best_move as determined by minimax
+    move_options = current_grid.empty_cell_list
+    scores = []
 
     if self.type == :human
       move = $stdin.getch.to_i  # Humans provide moves via the console.
       return move
     elsif self.type == :droid    # Droids use minimax/AI to determine their next move.
-      depth = 9 - current_grid.moves_remaining
-      score_array.each do |move_option|
+      depth = 9 - current_grid.empty_cell_list.count
+      move_options.each do |move_option|
+        scores[move_option] = 1 # minimax(self.designation, current_grid, depth)
+        puts "\nscores[#{move_option}] = #{scores[move_option]}\n"
 
       end
       # best_move = minimax(self, current_grid, depth)
-      best_move = score_array.each_with_index.max[1]
+      best_move = scores.each_with_index.max[1]
+      puts "\nbest_move = [#{scores.each_with_index.max[1]}]\n"
       return best_move
     end
   end
@@ -41,9 +44,9 @@ class Player
       then return score(current_grid)
     else # if this is not a terminal node, then dig deeper down the tree
       deeper_grid = current_grid.dup
-      depth = 9 - current_grid.empty_cell_list.count
       fake_move = deeper_grid.empty_cell_list.sample
       deeper_grid.add_move(current_player_designation, fake_move)
+      depth = 9 - current_grid.empty_cell_list.count
       current_score = -minimax(toggle(current_player_designation), deeper_grid, depth)
       return current_score
     end
