3aa10828ff7592f2cca13b84300263d85795c649
Raymond T. Hightower <rth@wisdomgroup.com>
2017-03-22 11:07:50 -0500

    Clarify comments on player type.

--- lib/player.rb
+++ lib/player.rb
@@ -3,7 +3,7 @@ require_relative './grid'
 class Player
 
   attr_accessor :designation, :type, :name
-  # :designation = X or O
+  # :designation = "X" or "O"
   # :type = :human or :droid
 
   def initialize(designation, type=:human, name="Random Name")
@@ -21,11 +21,12 @@ class Player
       return move
     elsif self.type == :droid    # Droids use minimax/AI to determine their next move.
       depth = 9 - current_grid.empty_cell_list.count
+
       move_options.each do |move_option|
         scores[move_option] = 1 # minimax(self.designation, current_grid, depth)
         puts "\nscores[#{move_option}] = #{scores[move_option]}\n"
-
       end
+
       # best_move = minimax(self, current_grid, depth)
       best_move = scores.each_with_index.max[1]
       puts "\nbest_move = [#{scores.each_with_index.max[1]}]\n"
