c62a57262dc658b2c61c8582070e917f20f87f77
Raymond T. Hightower <rth@wisdomgroup.com>
2017-06-30 07:59:59 -0500

    What have I done!?

--- lib/player.rb
+++ lib/player.rb
@@ -22,37 +22,36 @@ class Player
   def get_move(current_grid)
     if self.type == :human  # Humans provide moves via the console.
       move = $stdin.getch.to_i
-      return move
-
     elsif self.type == :droid    # Droids use minimax/AI to determine their next move.
-      best_move = 0    # best_move for the :droid will be determined by minimax
-      current_player_designation = self.designation
-      move_options = current_grid.empty_cell_list
-      depth = 9 - move_options.count
-
-      # Score each of the available moves and choose the highest scoring move.
-      # scorecard = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # Score each of the available moves and choose the highest scoring move.
-      scorecard = [-999, -999, -999, -999, -999, -999, -999, -999, -999, -999]  # Start by making all of the moves awful so that :droid won't consider them.
-
-      move_options.each do |fake_move|
-        fake_grid = current_grid.dupe
-        fake_grid.add_move(current_player_designation, fake_move)
-        puts "\n**** BEFORE minimax: fake_move = #{fake_move}, fake_grid = #{fake_grid.contents}\nscorecard[#{fake_move}] = #{scorecard[fake_move]}, scorecard = #{scorecard}\n"
-        puts "\n current_player = #{current_player_designation}." # current_grid, fake_grid below...\n"
-        # current_grid.print_color_grid
-        # fake_grid.print_color_grid
-        scorecard[fake_move] = minimax(current_player_designation, fake_grid, depth) # Go to the bottom of the tree.
-        puts "**** AFTER minimax: fake_move = #{fake_move}, fake_grid = #{fake_grid.contents}\nscorecard[#{fake_move}] = #{scorecard[fake_move]}, scorecard = #{scorecard}\n\n\n"
-      end
-
-      best_move = scorecard.each_with_index.max[1] # Should be making this choice at each level of the decision tree.
-
-      return best_move
+      move = minimax(current_player_designation, current_grid) # scorecard.each_with_index.max[1] # Should be making this choice at each level of the decision tree.
     end
+
+    return move
   end
 
   def minimax(player_designation, current_grid, depth)
-    puts "\nEntering minimax(mover = #{player_designation}, #{current_grid.contents}, depth = #{depth})\n"
+#   best_move = 0    # best_move for the :droid will be determined by minimax
+#   current_player_designation = self.designation
+#   move_options = current_grid.empty_cell_list
+#   depth = 9 - move_options.count
+
+#   # Score each of the available moves and choose the highest scoring move.
+#   # scorecard = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # Score each of the available moves and choose the highest scoring move.
+#   scorecard = [-999, -999, -999, -999, -999, -999, -999, -999, -999, -999]  # Start by making all of the moves awful so that :droid won't consider them.
+
+#   move_options.each do |fake_move|
+#     fake_grid = current_grid.dupe
+#     fake_grid.add_move(current_player_designation, fake_move)
+#     puts "\n**** BEFORE minimax: fake_move = #{fake_move}, fake_grid = #{fake_grid.contents}\nscorecard[#{fake_move}] = #{scorecard[fake_move]}, scorecard = #{scorecard}\n"
+#     puts "\n current_player = #{current_player_designation}." # current_grid, fake_grid below...\n"
+#     # current_grid.print_color_grid
+#     # fake_grid.print_color_grid
+#     scorecard[fake_move] = minimax(current_player_designation, fake_grid, depth) # Go to the bottom of the tree.
+#     puts "**** AFTER minimax: fake_move = #{fake_move}, fake_grid = #{fake_grid.contents}\nscorecard[#{fake_move}] = #{scorecard[fake_move]}, scorecard = #{scorecard}\n\n\n"
+
+#   move = scorecard.each_with_index.max[1] # Should be making this choice at each level of the decision tree.
+#   puts "\nEntering minimax(mover = #{player_designation}, #{current_grid.contents}, depth = #{depth})\n"
+
     # current_grid.print_color_grid
     if current_grid.terminal? then # if this is a terminal node, then return utility value
       this_score = score(current_grid, player_designation, depth)
