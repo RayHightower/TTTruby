54251f2c23f161a0370e78bcefdbd68477b3bc39
Raymond T. Hightower <rth@wisdomgroup.com>
2017-07-13 02:36:49 -0500

    Minimax jumps on opportunities to win. Now make it block when appropriate.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -79,59 +79,56 @@ describe Player do
       expect(move.class == Fixnum).to eq true
     end
 
-#    it "makes a legal move when given a grid with existing moves" do
-#      player = Player.new("O", :droid)
-#      grid = Grid.new
-#      grid.add_move("X", 1)  # XOX
-#      grid.add_move("O", 2)  #  X
-#      grid.add_move("X", 3)  #   O
-#      grid.add_move("O", 9)
-#      grid.add_move("X", 5)
-#      allowed = grid.empty_cell_list
-#      new_grid = grid.dupe
-#      legal_move = player.get_move(grid)
-#      # puts "\n** legal_move = #{legal_move}, allowed = #{allowed}\n"
-#      grid.print_color_grid
-#      grid.add_move(player.designation, legal_move)
-#      grid.print_color_grid
-#      expect(allowed.include? legal_move).to eq true
-#    end
+    it "makes a legal move when given a grid with existing moves" do
+      player = Player.new("O", :droid)
+      grid = Grid.new
+      grid.add_move("X", 1)  # XOX
+      grid.add_move("O", 2)  #  X
+      grid.add_move("X", 3)  #   O
+      grid.add_move("O", 9)
+      grid.add_move("X", 5)
+      allowed = grid.empty_cell_list
+      new_grid = grid.dupe
+      legal_move = player.get_move(grid)
+      grid.add_move(player.designation, legal_move)
+      expect(allowed.include? legal_move).to eq true
+    end
 
-    # it "returns score = 99 when :droid has won" do
-    #   player = Player.new("O", :droid)
-    #   grid = Grid.new
-    #   depth = 1
-    #   grid.add_move("O", 1)  # OOO
-    #   grid.add_move("O", 2)  #
-    #   grid.add_move("O", 3)  #
-    #   expect(player.score(grid, player.designation, depth)).to eq 99
-    # end
+    it "returns score = 999 when :droid has won" do
+      player = Player.new("O", :droid)
+      grid = Grid.new
+      depth = 1
+      grid.add_move("O", 1)  # OOO
+      grid.add_move("O", 2)  #
+      grid.add_move("O", 3)  #
+      expect(player.score(grid, player.designation)).to eq 999
+    end
 
-    # it "returns score = -99 when :droid has lost" do
-    #   player = Player.new("O", :droid)
-    #   grid = Grid.new
-    #   grid.add_move("X", 1)  # XXX
-    #   depth = 1
-    #   grid.add_move("X", 2)  #
-    #   grid.add_move("X", 3)  #
-    #   expect(player.score(grid, player.designation, depth)).to eq -99
-    # end
+    it "returns score = -999 when :droid has lost" do
+      player = Player.new("O", :droid)
+      grid = Grid.new
+      grid.add_move("X", 1)  # XXX
+      depth = 1
+      grid.add_move("X", 2)  #
+      grid.add_move("X", 3)  #
+      expect(player.score(grid, player.designation)).to eq -999
+    end
 
-    # it "retuns score = 0 when grid shows a tie game" do
-    #   player = Player.new("O", :droid)
-    #   grid = Grid.new
-    #   depth = 1
-    #   grid.add_move("O", 1)  # OXO
-    #   grid.add_move("X", 2)  # XOX
-    #   grid.add_move("O", 3)  # XOX
-    #   grid.add_move("X", 4)  #
-    #   grid.add_move("O", 5)  #
-    #   grid.add_move("X", 6)  #
-    #   grid.add_move("X", 7)  #
-    #   grid.add_move("O", 8)  #
-    #   grid.add_move("X", 9)  #
-    #   expect(player.score(grid, player.designation, depth)).to eq (0)
-    # end
+    it "retuns score = 0 when grid shows a tie game" do
+      player = Player.new("O", :droid)
+      grid = Grid.new
+      depth = 1
+      grid.add_move("O", 1)  # OXO
+      grid.add_move("X", 2)  # XOX
+      grid.add_move("O", 3)  # XOX
+      grid.add_move("X", 4)  #
+      grid.add_move("O", 5)  #
+      grid.add_move("X", 6)  #
+      grid.add_move("X", 7)  #
+      grid.add_move("O", 8)  #
+      grid.add_move("X", 9)  #
+      expect(player.score(grid, player.designation)).to eq (0)
+    end
 
 #    it "returns a blocking horizontal move when given a potential winning board for the opposing player" do
 #      grid = Grid.new
