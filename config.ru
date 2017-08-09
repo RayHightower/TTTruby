2101079c7d728cd48b422294d6dea927a5841722
Raymond T. Hightower <rth@wisdomgroup.com>
2017-08-09 18:05:02 -0500

    Use Rack::Reloader so we can change the GameWeb class without having to reload rack.

--- config.ru
+++ config.ru
@@ -1,6 +1,7 @@
 require 'rack'
 require_relative './lib/gameweb'
 
-use Rack::Reloader
+use Rack::Reloader, 0 # 10 second cooldown by default. Manually changed to zero.
 
-Rack::Handler::WEBrick.run GameWeb.new
+# Rack::Handler::WEBrick.run GameWeb.new
+run GameWeb.new
--- lib/gameweb.rb
+++ lib/gameweb.rb
@@ -3,9 +3,10 @@ require_relative './gamerunner'
 class GameWeb
 
   def call(env)  # To support Rack.
-    @gamerunner = GameRunner.new
+    # @gamerunner = GameRunner.new
     # Rack::Response.new("#{@gamerunner.game.grid.print_color_grid}")
     Rack::Response.new("Hello World!")
+    # [200, {"Content-Type" => "text/plain"}, ["Hello World!"]]
   end
 
 end
