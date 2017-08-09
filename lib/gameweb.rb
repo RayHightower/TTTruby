eb08b7ad3a34f0c2bfa49a104847a70d424bdb02
Raymond T. Hightower <rth@wisdomgroup.com>
2017-08-09 18:12:29 -0500

    Create template for the HTML for the game board.

--- lib/gameweb.rb
+++ lib/gameweb.rb
@@ -1,12 +1,19 @@
 require_relative './gamerunner'
+require "erb"
 
 class GameWeb
 
   def call(env)  # To support Rack.
     # @gamerunner = GameRunner.new
     # Rack::Response.new("#{@gamerunner.game.grid.print_color_grid}")
-    Rack::Response.new("Hello World!")
+    # Rack::Response.new("Hello World!")
+    Rack::Response.new(render("currentboard.html.erb"))
     # [200, {"Content-Type" => "text/plain"}, ["Hello World!"]]
+  end
+
+  def render(template)
+    path = File.expand_path("../views/#{template}", __FILE__)
+    ERB.new(File.read(path)).result(binding)
   end
 
 end
