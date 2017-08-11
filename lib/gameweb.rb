2d3200c524c8b65fbc89a07eae8a8464cc8cb907
Raymond T. Hightower <rth@wisdomgroup.com>
2017-08-11 16:36:27 -0500

    What is the best way to pass parameters between the core and the view?

--- lib/gameweb.rb
+++ lib/gameweb.rb
@@ -3,6 +3,11 @@ require "erb"
 
 class Gameweb
 
+  def initialize
+    @gamerunner = GameRunner.new
+    @gridcontents = @gamerunner.game.grid.contents
+  end
+
   def call(env)  # To support Rack.
     request = Rack::Request.new(env)
     # request.path # = the URL path that the user typed in to access this page
@@ -11,7 +16,6 @@ class Gameweb
   end
 
   def render(template)
-    @gamerunner = GameRunner.new
     path = File.expand_path("../views/#{template}", __FILE__)
     ERB.new(File.read(path)).result(binding)
   end
